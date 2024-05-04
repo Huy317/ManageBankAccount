0fa7347992c2df0a5990b69109e5dbf2c9151f13
Huy317 <167590855+Huy317@users.noreply.github.com>
2024-05-04 09:11:46 +0700

    done q2

---
+++ src/main/java/com/mycompany/managebankaccount/BankAccount.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.managebankaccount;
+
+/**
+ *
+ * @author Student
+ */
+public class BankAccount {
+    private int mAccNum;
+    private String mFullName;
+    private double mBalance;
+
+    public BankAccount(int mAccNum, String mFullName, double mBalance) {
+        this.mAccNum = mAccNum;
+        this.mFullName = mFullName;
+        this.mBalance = mBalance;
+    }
+
+    public BankAccount() {
+    }
+
+    public int getAccNum() {
+        return mAccNum;
+    }
+
+    public String getFullName() {
+        return mFullName;
+    }
+
+    public double getBalance() {
+        return mBalance;
+    }
+    public void deposit(double money){
+        mBalance += money;
+    }
+    public boolean withdraw(double money){
+        if (mBalance >= money){
+            mBalance -= money;
+            return true;
+        }else{
+            return false;
+        }
+    }    
+    public boolean transferMoney(BankAccount acc, double money){
+        if (mBalance >= money){
+            acc.deposit(money);
+            mBalance -= money;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void print(){
+        System.out.println("Acc Num: "+mAccNum+"\nFullName: "+mFullName+"\nBalance: "+mBalance);
+    }
+    
+}
--- src/main/java/com/mycompany/managebankaccount/ManageBankAccount.java
+++ src/main/java/com/mycompany/managebankaccount/ManageBankAccount.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.managebankaccount;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Student
@@ -11,6 +14,81 @@ package com.mycompany.managebankaccount;
 public class ManageBankAccount {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+          Scanner sc = new Scanner(System.in);
+          ArrayList<BankAccount> accList = new ArrayList<>();
+          int n = sc.nextInt();
+          for (int i = 0; i < n; i++){
+              System.out.print("Account Number: ");
+              int accNum = sc.nextInt();
+              System.out.print("FullName: ");
+              String fullName = sc.nextLine();
+              sc.nextLine();
+              System.out.print("Balance: ");
+              double balance = sc.nextDouble();
+              accList.add(new BankAccount(accNum,fullName,balance));
+          }
+          for (int i = 0; i<n;i++){
+              accList.get(i).print();
+          }
+          System.out.print("Account number to deposit: ");
+          int num = sc.nextInt();
+          boolean found = false;
+          for (int i = 0; i<n;i++){
+              if (num == accList.get(i).getAccNum()){
+                  System.out.print("Enter deposit amount: ");
+                  double money = sc.nextDouble();
+                  accList.get(i).deposit(money);
+                  found = true;
+                  break;
+              }
+          }
+          if (!found){
+              System.out.println("Couldn't find account number");
+          }
+          
+          System.out.print("Account number to withdraw: ");
+          num = sc.nextInt();
+          found = false;
+          for (int i = 0; i<n;i++){
+              if (num == accList.get(i).getAccNum()){
+                  System.out.print("Enter withdraw amount: ");
+                  double money = sc.nextDouble();
+                  if (accList.get(i).withdraw(money) == true){
+                      System.out.println("Withdrawal Successful");
+                  }else{
+                      System.out.println("Withdrawal Unsuccessful");
+                  }
+                  found = true;
+                  break;
+              }
+          }
+          if (!found){
+              System.out.println("Couldn't find account number");
+          }
+          
+          System.out.print("Choose an account number to do the transfer: ");
+          num = sc.nextInt();
+          int i1 = 0;
+          for (int i = 0; i<n;i++){
+              if ((num == accList.get(i).getAccNum())){
+                  i1 = i;
+                  break;
+              }
+          }
+          System.out.print("Choose an account number to receive the transfer: ");
+          num = sc.nextInt();
+          int i2 = 0;
+          for (int i = 0; i<n;i++){
+              if ((num == accList.get(i).getAccNum())){
+                  i2 = i;
+                  break;
+              }
+          }
+          System.out.print("Enter money to transfer: ");
+          double money = sc.nextDouble();
+          
+         accList.get(i1).transferMoney(accList.get(i2), money);
+         accList.get(i1).print();
+         accList.get(i2).print();
     }
 }
