c36266e4fc00a0a3ebbfe17eb06be8d8102fe187
Huy317 <167590855+Huy317@users.noreply.github.com>
2024-05-04 08:15:18 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/managebankaccount/ManageBankAccount.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.managebankaccount;
+
+/**
+ *
+ * @author Student
+ */
+public class ManageBankAccount {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
